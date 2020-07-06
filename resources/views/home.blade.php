4613bd73e51c415b0bb1c82a67d1f2a6e418b5f0
wildan24 <wildanabdurrohman11@gmail.com>
2020-07-06 22:24:09 +0700

    first-templating master

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('template.master')
+
+@section('content')
+    <div class="card">
+        <div class="card-header">
+            <h4>Judul Modul</h4>
+        </div>
+        <div class="car-body">
+            <h4>Halaman Kosong</h4>
+            <img src="webfonts/kosong.jpg" alt="">
+        </div>
+    </div>
+@endsection
